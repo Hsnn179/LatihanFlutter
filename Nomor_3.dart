67e0029ecf1476301c94c51b2d04a9b2a82674c8
Hsnn179 <103586938+Hsnn179@users.noreply.github.com>
2022-04-21 18:51:37 +0700

    Delete Nomor_3.dart

--- Nomor_3.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-void main(List<String> args) 
-{
-  stdout.write("Masukkan nama depan : ");
-  String? inputnd = stdin.readLineSync();
-  stdout.write("Masukkan nama belakang : ");
-  String? inputnb = stdin.readLineSync();
-  print("Nama lengkap : ${inputnd} ${inputnb}");
-}
